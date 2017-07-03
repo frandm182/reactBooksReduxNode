822a4315d4436a4fcda5e088d9fa244f4af88bc4
Francisco Diaz <franciscojavier.diaz@i3television.es>
2017-07-03 14:54:28 +0200

    Crud Completed

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ const reducer = (state={books: []}, action) => {
         case 'POST_BOOK':
             let books = state.books.concat(action.payload)
             return {books: [...state.books,...action.payload]};
+        case 'DELETE_BOOK':
+            const currentBookToDelete = [...state.books]
+            const indexToDelete = currentBookToDelete.findIndex(book => book.id === action.payload.id)
+            return { books: [...currentBookToDelete.slice(0, indexToDelete), ...currentBookToDelete.slice(indexToDelete + 1)]};
+        case 'UPDATE_BOOK':
+            const currentBookToUpdate = [...state.books]
+            const indexToUpdate = currentBookToUpdate.findIndex(book => book.id === action.payload.id);
+            const newBookToUpdate = {...currentBookToUpdate[indexToUpdate], title: action.payload.title};
+            console.log(newBookToUpdate);
+            return { books: [...currentBookToUpdate.slice(0, indexToUpdate), newBookToUpdate, ...currentBookToUpdate.slice(indexToUpdate + 1)]};
+        
     }
     return state;
 }
@@ -22,7 +33,12 @@ store.dispatch({
         title: 'First book',
         description: 'Description of first book',
         price: 33.98
-    },
+    }]    
+})
+
+store.dispatch({
+    type: 'POST_BOOK',
+    payload: [
     {
         id: 2,
         title: 'Second book',
@@ -34,10 +50,23 @@ store.dispatch({
 store.dispatch({
     type: 'POST_BOOK',
     payload: [{
-        id: 1,
-        title: 'First book',
-        description: 'Description of first book',
-        price: 33.98
+        id: 3,
+        title: 'Third book',
+        description: 'Description of third book',
+        price: 15.30
     }]    
+})
+
+store.dispatch({
+    type: 'DELETE_BOOK',
+    payload: { id: 2 }    
+})
+
+store.dispatch({
+    type: 'UPDATE_BOOK',
+    payload: { 
+         id: 3,
+        title: 'Third book changed'
+     }    
 })
 
